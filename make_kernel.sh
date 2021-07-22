d6e1e6bfbaec28d72d7cd4b325716d7b30b798c0
antman666 <945360554@qq.com>
2021-07-22 23:29:17 +0800

    update linux-ck-uksm to 5.12.19-2

--- make_kernel.sh
+++ make_kernel.sh
@@ -1,5 +1,5 @@
 linux_ver=5.12.19
-linux_rel=1
+linux_rel=2
 _subarch=30
 _gcc_more_v=20210610
 _major=5.12
@@ -20,6 +20,8 @@ wget -c ${_patches_url}/futex2-stable-patches-v7/0001-futex2-resync-from-gitlab.
 wget -c ${_patches_url}/lru-patches-v4/0001-lru-patches.patch
 wget -c ${_patches_url}/zstd-patches-v2/0001-zstd-patches.patch
 wget -c ${_patches_url}/initramfs-patches/0001-initramfs-patches.patch
+wget -c ${_patches_url}/android-patches/0001-android-export-symbold-and-enable-building-ashmem-an.patch
+wget -c ${_patches_url}/compaction-patches/0001-compaction-patches.patch
 
 tar -xpvf linux-${linux_ver}.tar.xz
 tar -xpvf ${_gcc_more_v}.tar.gz
